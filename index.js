91f76a0e2315a86205411784b489465bc1a0fb4e
Daniel <daniel@djsmith.uk>
2022-04-08 01:50:26 +0100

    modern rewrite of yamb

---
+++ index.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const Client = require('./client/Client');
+const config = require('./config.json');
+const {Player} = require('discord-player');
+
+const client = new Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+console.log(client.commands);
+
+const player = new Player(client);
+
+player.on('error', (queue, error) => {
+  console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+});
+
+player.on('connectionError', (queue, error) => {
+  console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+});
+
+player.on('trackStart', (queue, track) => {
+  queue.metadata.send(`Now playing: **${track.title}**`);
+});
+
+player.on('trackAdd', (queue, track) => {
+  queue.metadata.send(`**${track.title}** has been added to the queue.`);
+});
+
+player.on('botDisconnect', queue => {
+  queue.metadata.send('Someone kicked me from the party, clearing the queue.');
+});
+
+player.on('channelEmpty', queue => {
+  queue.metadata.send('Nobody\'s home... time to leave.');
+});
+
+player.on('queueEnd', queue => {
+  queue.metadata.send('Your jamming session is over partner.');
+});
+
+client.once('ready', async () => {
+  console.log('Ready!');
+});
+
+client.on('ready', function() {
+  client.user.setActivity(config.activity, { type: config.activityType });
+});
+
+client.once('reconnecting', () => {
+  console.log('Reconnecting!');
+});
+
+client.once('disconnect', () => {
+  console.log('Disconnect!');
+});
+
+client.on('messageCreate', async message => {
+  if (message.author.bot || !message.guild) return;
+  if (!client.application?.owner) await client.application?.fetch();
+
+  if (message.content === '!deploy' && message.author.id === client.application?.owner?.id) {
+    await message.guild.commands
+      .set(client.commands)
+      .then(() => {
+        message.reply('Deployed!');
+      })
+      .catch(err => {
+        message.reply('Could not deploy commands! Make sure the bot has the application.commands permission!');
+        console.error(err);
+      });
+  }
+});
+
+client.on('interactionCreate', async interaction => {
+  const command = client.commands.get(interaction.commandName.toLowerCase());
+
+  try {
+    if (interaction.commandName == 'ban' || interaction.commandName == 'userinfo') {
+      command.execute(interaction, client);
+    } else {
+      command.execute(interaction, player);
+    }
+  } catch (error) {
+    console.error(error);
+    interaction.followUp({
+      content: 'There was an error trying to execute that command!',
+    });
+  }
+});
+
+client.login(config.token);
